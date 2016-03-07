f13148ebf9ce4a6be091a4ee22a5834ae347089d
Josif78 <seph12.alc@gmail.com>
2016-03-08 05:51:06 +0900

    function now reads input and checks error cases

--- cExLib.h
+++ cExLib.h
@@ -2,9 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+	//helper function used to call getHammingDistance
 void hammingDistance () {
+	int maxLen = 100;	//defines max string length
+	char str1[maxLen], str2[maxLen];
+	int retVal;
 
-	int retVal = getHammingDistance();
+	printf("Input #1: ");
+	fgets(str1, maxLen, stdin);
+    str1[strlen(str1)-1] = '\0';
+
+	printf("Input #2: ");
+	fgets(str2, maxLen, stdin);
+    str2[strlen(str2)-1] = '\0';
+
+	retVal = getHammingDistance(str1, str2);
+
+		//used switch case to check for error cases
 	switch (retVal) {
 		case -1: 
 			printf("Error! String Lengths are not equal!");
@@ -18,7 +32,17 @@ void hammingDistance () {
 	printf("\n");
 }
 
-int getHammingDistance () {
-	int val = 0
+int getHammingDistance (char *str1, char *str2) {
+	int val = 0;
+
+	if (!strlen(str1) || !strlen(str2)) {
+		return -2;
+	}
+	if (strlen(str1) != strlen(str2)) {
+		return -1;
+	}
+	
+	printf("1: %s; 2: %s\n", str1, str2);
+
 	return val;
 }

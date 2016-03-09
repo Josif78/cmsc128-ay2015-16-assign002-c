8824cd21f5ba2f38362b61a9f48d351a9b7af999
Josif78 <seph12.alc@gmail.com>
2016-03-09 10:36:17 +0900

    added helper functions for skew so theycan be called individually

--- cExLib.h
+++ cExLib.h
@@ -10,6 +10,12 @@ int getSubstrPattern(char *str1, char *str2);
 int isStringValid(char *str1, char *str2);
 int getTypeSkew(char *str1, char *str2, int type);
 
+int getSkew(char *str1, char *str2);
+int getMaxSkew(char *str1, char *str2);
+int getMinSkew(char *str1, char *str2);
+
+	//function used to accept input 
+		//and print results returned by main functions
 void getStrings (int choice) {
 	int maxLen = 100;	//defines max string length
 	int retVal;
@@ -27,6 +33,10 @@ void getStrings (int choice) {
     if (!strlen(str1) || !strlen(str2)) {
     	printf("Missing Input!");
     }
+    if (!1) {
+		printf("Invalid input");
+	}
+
     else {
 	    switch (choice) {
 	    		//checks cases for hamming distance and
@@ -56,27 +66,21 @@ void getStrings (int choice) {
 				}
 				printf("Invalid");
 				break;
-
+				//skew function
 			case 4: 
-				skip+=1;
+				printf("Value: %d", getSkew(str1, str2));
+				break;
 			case 5:
-				skip+=1;
+				printf("Value: %d", getMaxSkew(str1, str2));
+				break;
 			case 6:
-				skip+=1;
-				if (1) {
-					printf("Value: %d", getTypeSkew(str1, str2, skip));
-				}
-				else {
-					printf("Invalid input");
-				}
+				printf("Value: %d", getMinSkew(str1, str2));
 				break;
 			default:
-				printf("Nepu-nepu");
+				printf("No match found");
 	    }
 	}
     printf("\n");
-
-    skip = 0;
 }
 
 
@@ -84,11 +88,11 @@ void getStrings (int choice) {
 int getHammingDistance (char *str1, char *str2) {
 	int val = 0;
 	int i;
-
+		//checks if string lengths don't match
 	if (strlen(str1) != strlen(str2)) {
 		return -1;
 	}
-
+		//gets difference in values
 	for (i=0; i<strlen(str1); i+=1) {
 		if(str1[i] != str2[i]) {
 			val+=1;
@@ -125,17 +129,22 @@ int getSubstrPattern (char *str1, char *str2) {
 
 	return val;
 }
-
+	
 int isStringValid (char *str1, char *str2) {
 	int i, j;
 	int check = 1;
 
+		//loops at each letter of the first string
 	for (i=0; i<strlen(str1); i+=1) {
+			//loops at each letter of the "alphabet" to check if part
 		for (j=0; j<strlen(str2); j+=1) {
+				//if a match is found from the string
 			if (str1[i] == str2[j]) {
 				check = 0;
 			}
 		}
+			//if no matching string is found in
+				//the "alphabet", returns 0 or invalid
 		if (check) {
 			return 0;
 		}
@@ -143,13 +152,14 @@ int isStringValid (char *str1, char *str2) {
 	}
 	return 1;
 }
-
+	//general skew function
 int getTypeSkew (char *str1, char *str2, int type) {
 	int num = atoi(str2);
 	int max = 0, min = 0;
 	int i;
 	int val = 0;
 
+		//checks for g's or c's
 	for (i=0; i<num; i+=1) {
 		if (str1[i] == 'G' || str1[i] == 'g') {
 			val+=1;
@@ -157,18 +167,35 @@ int getTypeSkew (char *str1, char *str2, int type) {
 		if (str1[i] == 'C' || str1[i] == 'c') {
 			val-=1;
 		}
+			//changes max value if exceeded by current value
 		if (max < val) {
 			max = val;
 		}
+			//changes min value if exceeded by current value
 		if (min > val) {
 			min = val;
 		}
 	}
-	if (type == 2) {
+
+	if (type == 1) {
 		return max;
 	}
-	if (type == 1) {
+	if (type == 2) {
 		return min;
 	}
 	return val;
+}
+
+	//helper skew functions
+int getSkew(char *str1, char *str2) {
+		//type 0 is normal skew function
+	getTypeSkew(str1, str2, 0);
+}
+int getMaxSkew(char *str1, char *str2) {
+		//type 1 is get max value of skew
+	getTypeSkew(str1, str2, 1);
+}
+int getMinSkew(char *str1, char *str2) {
+		//type 2 is get min value of skew
+	getTypeSkew(str1, str2, 2);
 }
--- printer.c
+++ printer.c
@@ -9,28 +9,31 @@ main() {
 	char str[maxLen];
 
 	while (1) {
-		printf("[1] Hamming Distance\n");
+		printf("\n*** Menu ***\n");
+		printf("[1] Get Hamming Distance\n");
 		printf("[2] Get Substring Pattern\n");
-		printf("[3] Check String Validity\n");
+		printf("[3] Get String Validity\n");
 		printf("[4] Get Skew\n");
 		printf("[5] Get Max Skew\n");
 		printf("[6] Get Min Skew\n");
 		printf("[7] Exit\n");
 		printf("Choice: ");
+
 		fgets(str, maxLen, stdin);
-		if (strlen(str) > 2) {
+		if (strlen(str) > 2 || strlen(str) < 2) {
 			printf("Invalid choice\n\n");
 			continue;
     	}
-    	
+
     	str[2] = '\0';
     	choice = atoi(str);
 
 		if (choice == 7) {
-			printf("For Nohr!\n");
+			printf("Exited Program\n");
 			break;
 		}
 		getStrings(choice);
 	}
+	printf("\nFor Nohr!\n");
 	return 78;
 }
